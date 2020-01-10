4014c00cfcd0ad01e6e49b5cfba6d60ea0c21cec
ThomasSalcher <salcher25@gmail.com>
2020-01-10 12:50:52 +0100

    XamrainAPP
    initial Commit

---
+++ Xamarin-APP/Xamrain-APP/Xamrain-APP/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamrain_APP.Services;
+using Xamrain_APP.Views;
+
+namespace Xamrain_APP
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Xamarin-APP/Xamrain-APP/Xamrain-APP/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamrain_APP.Models;
+
+namespace Xamrain_APP.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
